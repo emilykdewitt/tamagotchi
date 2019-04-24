9034a0bdb9d087fbf6226df4f352810ae537d708
Emily DeWitt <emilykdewitt@gmail.com>
2019-04-23 21:34:22 -0500

    added styling and positioning of image

--- src/javascripts/components/tamagotchi/tamagotchi.js
+++ src/javascripts/components/tamagotchi/tamagotchi.js
@@ -6,16 +6,28 @@ const printToDomFunction = util.printToDom;
 
 const quadrantInfo = quadrantData.getQuadrantData();
 
+const eatScore = 0;
+const playScore = 0;
+const fightScore = 0;
+const sleepScore = 0;
+
 const domStringBuilder = () => {
   quadrantInfo.forEach((quadrant) => {
     let domString = '';
     domString += '<div class="quadInfo">';
     domString += `<h2>${quadrant.id}</h2>`;
-    domString += `<button id="${quadrant.button1}">${quadrant.button1}</button>`;
+    domString += `<button id="${quadrant.button1}" class="${quadrant.id}">${quadrant.button1}</button>`;
     domString += `<button id="${quadrant.button2}">${quadrant.button2}</button>`;
     domString += `<h3>${quadrant.meter}: </h3>`;
-    domString += `<div id="${quadrant.id}Score">`;
-    domString += '</div>';
+    if (quadrant.id === 'eat') {
+      domString += `<h3 id="eatScore">${eatScore}</h3>`;
+    } else if (quadrant.id === 'play') {
+      domString += `<h3 id="playScore">${playScore}</h3>`;
+    } else if (quadrant.id === 'fight') {
+      domString += `<h3 id="fightScore">${fightScore}</h3>`;
+    } else {
+      domString += `<h3 id="sleepScore">${sleepScore}</h3>`;
+    }
     printToDomFunction(quadrant.id, domString);
   });
 };
